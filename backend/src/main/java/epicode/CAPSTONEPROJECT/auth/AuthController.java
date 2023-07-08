503f67e5d4811d32d14d0961e70239a5ca01f3b0
Gianlucapratico <123976541+Gianlucapratico@users.noreply.github.com>
2023-07-08 10:55:15 +0200

    Sign In funzionante

--- backend/src/main/java/epicode/CAPSTONEPROJECT/auth/AuthController.java
+++ backend/src/main/java/epicode/CAPSTONEPROJECT/auth/AuthController.java
@@ -1,9 +1,12 @@
 package epicode.CAPSTONEPROJECT.auth;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +19,7 @@ import epicode.CAPSTONEPROJECT.exceptions.NotFoundException;
 import epicode.CAPSTONEPROJECT.exceptions.UnauthorizedException;
 import epicode.CAPSTONEPROJECT.payloads.UserCreatePayload;
 import epicode.CAPSTONEPROJECT.payloads.UserLoginPayload;
+import epicode.CAPSTONEPROJECT.repositories.UsersRepository;
 import epicode.CAPSTONEPROJECT.services.UsersService;
 
 @RestController
@@ -24,21 +28,65 @@ public class AuthController {
 
 	@Autowired
 	UsersService usersService;
+	@Autowired
+	private UsersRepository usersRepo;
 
 	@Autowired
 	private PasswordEncoder bcrypt;
 
 	// @Autowired
 	// RoleRepository roleRepo;
+	private boolean isValidEmail(String email) {
+		// Utilizza espressioni regolari per la validazione dell'indirizzo email
+		String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+		return email.matches(emailRegex);
+	}
 
 	@PostMapping("/register")
-	public ResponseEntity<User> register(@RequestBody @Validated UserCreatePayload body) {
+	public ResponseEntity<?> register(@RequestBody @Validated UserCreatePayload body) {
+		try {
+			// Verifica se tutti i campi sono stati inseriti
+			if (StringUtils.isEmpty(body.getNome()) || StringUtils.isEmpty(body.getNome())
+					|| StringUtils.isEmpty(body.getUsername()) || StringUtils.isEmpty(body.getEmail())
+					|| StringUtils.isEmpty(body.getPassword())) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Devi inserire tutti i campi");
+			}
 
-		body.setPassword(bcrypt.encode(body.getPassword()));
+			// Verifica la validità dell'indirizzo email
+			if (!isValidEmail(body.getEmail())) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email non valida");
+			}
 
-		User createdUser = usersService.create(body);
+			// Verifica se l'indirizzo email è già in uso
+			Optional<User> existingUser = usersRepo.findByEmail(body.getEmail());
+			if (existingUser.isPresent()) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("L'indirizzo email è già in uso");
+			}
 
-		return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+			// Verifica la lunghezza del nome
+			if (body.getNome().length() < 3) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il nome deve avere almeno 3 caratteri");
+			}
+			// Verifica la lunghezza dello username
+			if (body.getUsername().length() < 3) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Lo username deve avere almeno 3 caratteri");
+			}
+
+			// Verifica la lunghezza della password
+			if (body.getPassword().length() < 5) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La password deve avere almeno 5 caratteri");
+			}
+
+			// Aggiungi altre logiche di validazione se necessario
+
+			body.setPassword(bcrypt.encode(body.getPassword()));
+			User createdUser = usersService.create(body);
+
+			return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Si è verificato un errore durante la registrazione");
+		}
 	}
 
 	@PostMapping("/login")
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -16,9 +16,26 @@ const SignIn = () => {
     e.preventDefault();
 
     if (!name || !surname || !username || !email || !password) {
-      setAlert(
-        "Errore durante la registrazione: tutti i campi sono obbligatori"
-      );
+      setAlert("Devi inserire tutti i campi");
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      setAlert("Email non valida");
+      return;
+    }
+
+    if (name.length < 3) {
+      setAlert("Il nome deve avere almeno 3 caratteri");
+      return;
+    }
+    if (username.length < 3) {
+      setAlert("Lo username deve avere almeno 3 caratteri");
+      return;
+    }
+
+    if (password.length < 5) {
+      setAlert("La password deve avere almeno 5 caratteri");
       return;
     }
 
@@ -45,27 +62,18 @@ const SignIn = () => {
       });
 
       if (response.ok) {
-        // Registrazione avvenuta con successo
-        setAlert("Registrazione avvenuta con successo");
+        setAlert(null); // Rimuovi l'alert precedente
         setRedirect(true); // Imposta il valore di "redirect" a true
 
         setEmailError("");
         // Effettua eventuali azioni aggiuntive o reindirizzamenti
       } else {
-        // Errore nella registrazione
         const errorData = await response.json();
-        if (errorData && errorData.errors) {
-          const errorMessage = Object.values(errorData.errors).join(". ");
-          setAlert(`Errore durante la registrazione: ${errorMessage}`);
-        }
-        //if errorMessage == ("Email already in use!")
         if (
           errorData &&
-          errorData.message &&
-          errorData.message.includes("Email already in use!")
+          errorData.message === "L'indirizzo email è già in uso"
         ) {
-          const errorMessage = `L'indirizzo email ${email} è già in uso`;
-          setAlert(errorMessage);
+          setAlert(`L'indirizzo email ${email} è già in uso`);
         } else if (errorData && errorData.errors) {
           const errorMessage = Object.values(errorData.errors).join(". ");
           setAlert(`Errore durante la registrazione: ${errorMessage}`);
@@ -78,6 +86,12 @@ const SignIn = () => {
       setAlert("Si è verificato un errore durante la registrazione");
     }
   };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/;
+    return emailRegex.test(email);
+  };
+
   if (redirect) {
     navigate("/login"); // Naviga alla pagina di login
   }
@@ -88,15 +102,11 @@ const SignIn = () => {
         {alert && (
           <div
             className={`alert ${
-              alert.startsWith("Errore") ? "alert-danger" : "alert-success"
+              alert ? "alert-danger" : "alert-success"
             } d-flex justify-content-center align-items-center`}
             role="alert"
           >
-            {alert.startsWith("Errore") ? (
-              <span>{alert}</span>
-            ) : (
-              <span>Registrazione avvenuta con successo!</span>
-            )}
+            <span>{alert}</span>
           </div>
         )}
         <div className="row justify-content-center">
